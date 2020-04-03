f72d525540de639a2174b3450a14e96acca468bc
junya-gera <aa106921@gmail.com>
2020-04-03 22:02:12 +0900

    add side-menu function

--- app/assets/javascripts/bars.js
+++ app/assets/javascripts/bars.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  
+  $('#bars').on('click',function(){
+    $('body').addClass('menu--open')
+  });
+
+  $('#close').on('click',function(){
+    $('body').removeClass('menu--open')
+  })
 });
