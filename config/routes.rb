1c1c59db9cad07018f509d3bf8d6e6ad56099a91
junya-gera <aa106921@gmail.com>
2020-04-03 18:33:06 +0900

    add image column

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    root :to => "devise/registrations#new"
-  end
+  root :to => "tasks#index"
+  resources :tasks, only: [:new, :create, :index]
 end
