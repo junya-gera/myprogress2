bc12f238e6dbfb04266674b09d344646bb7a5c5d
junya-gera <aa106921@gmail.com>
2020-04-05 00:12:00 +0900

    add user show scss

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "tasks#index"
   resources :tasks, only: [:new, :create, :index]
+  resources :users, only: [:show, :edit, :update]
 end
