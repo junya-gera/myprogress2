3bd402031d922ab98cff46bc91614e83fa89a88e
junya-gera <aa106921@gmail.com>
2020-04-09 01:13:17 +0900

    fix btn text

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -17,7 +17,18 @@ $(function () {
       left: 'prev,next today',
       center: 'title',
       right: 'month,agendaWeek,agendaDay'
-  },
+    },
+    buttonText: {
+      prev: '前',
+      next: '次',
+      prevYear: '前年',
+      nextYear: '翌年',
+      today: '今日',
+      month: '月',
+      week: '週',
+      day: '日'
+      },
+
   
   });
   
