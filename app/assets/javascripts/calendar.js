dbd905940292c09accc3eccd268c4a3fa8b57836
junya-gera <aa106921@gmail.com>
2020-04-09 01:35:26 +0900

    fix text to Japanese

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -19,8 +19,6 @@ $(function () {
       right: 'month,agendaWeek,agendaDay'
     },
     buttonText: {
-      prev: '前',
-      next: '次',
       prevYear: '前年',
       nextYear: '翌年',
       today: '今日',
@@ -28,7 +26,14 @@ $(function () {
       week: '週',
       day: '日'
       },
-      height: 650
+      height: 650,
+      locale: 'ja',
+      monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+      monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+
+
   
   });
   
