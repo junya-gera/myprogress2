07f2d30b32b5582434ceaab7a0c56a03fbb65f5b
junya-gera <aa106921@gmail.com>
2020-04-10 22:25:39 +0900

    routing nest tasks to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "tasks#index"
-  resources :tasks, only: [:new, :create, :index]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resources :tasks, only: [:new, :create, :index]
+  end
+
 end
