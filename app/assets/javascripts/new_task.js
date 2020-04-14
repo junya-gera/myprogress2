930362fabaf9c43817a5fb13ddc76759bb7b21e2
junya-gera <aa106921@gmail.com>
2020-04-14 23:13:53 +0900

    returned done

--- app/assets/javascripts/new_task.js
+++ app/assets/javascripts/new_task.js
@@ -1,14 +1,24 @@
   
-$('#new_event').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+  $('#new_event').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.event__units').append(html);
+      $('#event_title').val('');
+      $('#event_content').val('');
+      $('.event__form--btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
