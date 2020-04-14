41854851f90ba48c9b1a0af63816fb4f4efc8572
junya-gera <aa106921@gmail.com>
2020-04-14 23:04:20 +0900

    delete done and create.js.erb

--- app/assets/javascripts/new_task.js
+++ app/assets/javascripts/new_task.js
@@ -1,24 +1,14 @@
   
-  $('#new_event').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.event__units').append(html);
-      $('#event_title').val('');
-      $('#event_content').val('');
-      $('.event__form--btn').prop('disabled', false);
-    })
-    .fail(function(){
-      alert('error');
-    })
+$('#new_event').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
+})
