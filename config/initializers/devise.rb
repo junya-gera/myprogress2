824a7e9d60c4a9f9733aee4dc4d801c623a344f7
junya-gera <aa106921@gmail.com>
2020-04-18 04:40:16 +0900

    add config.omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,8 @@
 Devise.setup do |config|
   config.scoped_views = true
   config.omniauth :facebook,ENV['FACEBOOK_CLIENT_ID'],ENV['FACEBOOK_CLIENT_SECRET']
+  config.omniauth :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET']
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
