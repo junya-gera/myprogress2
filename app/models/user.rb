608e1a975ec6cb57f216047eec1c461dec30e0a2
junya-gera <aa106921@gmail.com>
2020-04-17 10:38:10 +0900

    add user.from_omniauth method to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,19 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :events
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
+
+
 end
