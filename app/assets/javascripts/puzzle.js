e703475655d2e7e7b883608e9da37bfb6036a9b5
junya-gera <aa106921@gmail.com>
2020-04-19 16:13:29 +0900

    add modal-close

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -1,6 +1,11 @@
 $(document).on("turbolinks:load", function() {
-  
+
   $('#task-modal-show').on('click', function(){
     $('.task-modal').fadeIn(250);
   })
+
+  $('#modal-close').on('click', function(){
+    $('.task-modal').fadeOut(250);
+  })
+
 })
