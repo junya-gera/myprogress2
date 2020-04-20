68418fd2a96310a33bc53b4b92a005d23f59c0c3
junya-gera <aa106921@gmail.com>
2020-04-20 20:35:42 +0900

    add puzzle create action

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -4,4 +4,18 @@ class PuzzlesController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def create
+    @puzzle = Puzzle.new(puzzle_params)
+    @puzzle.save
+    redirect_to root_path
+  end
+  
+private
+  def puzzle_params
+    params.require(:puzzle).permit(:task).merge(user_id: current_user.id)
+  end
+  
+end
+
+
 end
