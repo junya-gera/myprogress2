781561a0785e0d2084138ce98b8cdcccb8af06e4
junya-gera <aa106921@gmail.com>
2020-04-22 16:13:59 +0900

    add puzzle-checked-btn

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -8,7 +8,7 @@ $(document).on("turbolinks:load", function() {
     $('.task-modal').fadeOut(250);
   })
 
-  
+
   const image = new Image();
   image.onload = function(e){
     const canvas = document.getElementById("stage");
@@ -89,5 +89,88 @@ $(document).on("turbolinks:load", function() {
   };
     
   image.src = "/assets/usagi2.png";
+
+  $('#task-btn-1').click(function(){
+    let task1 = $('#input-task-1').val();
+    $('#task-show-1').text(task1);
+  });
+  $('.BC-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#top-left').toggle('shake');
+  });
+
+  $('#task-btn-2').click(function(){
+    let task2 = $('#input-task-2').val();
+    $('#task-show-2').text(task2);
+  });
+  $('.TR-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#top-center').toggle('shake');
+  });
+
+  $('#task-btn-3').click(function(){
+    let task3 = $('#input-task-3').val();
+    $('#task-show-3').text(task3);
+  });
+  $('.CL-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#top-right').toggle('shake');
+  });
+
+  $('#task-btn-4').click(function(){
+    let task4 = $('#input-task-4').val();
+    $('#task-show-4').text(task4);
+  });
+  $('.CC-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#center-left').toggle('shake');
+  });
+
+  $('#task-btn-5').click(function(){
+    let task5 = $('#input-task-5').val();
+    $('#task-show-5').text(task5);
+  });
+  $('.BL-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#center-center').toggle('shake');
+  });
+
+  $('#task-btn-6').click(function(){
+    let task6 = $('#input-task-6').val();
+    $('#task-show-6').text(task6);
+  });
+  $('.TL-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#center-right').toggle('shake');
+  });
+
+  $('#task-btn-7').click(function(){
+    let task7 = $('#input-task-7').val();
+    $('#task-show-7').text(task7);
+  });
+  $('.BR-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#bottom-left').toggle('shake');
+  });
+
+  $('#task-btn-8').click(function(){
+    let task8 = $('#input-task-8').val();
+    $('#task-show-8').text(task8);
+  });
+  $('.TC-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#bottom-center').toggle('shake');
+  });
+
+  $('#task-btn-9').click(function(){
+    let task9 = $('#input-task-9').val();
+    $('#task-show-9').text(task9);
+  });
+  $('.CR-btn').click(function(){
+    $(this).toggleClass('is-checked');
+    $('#bottom-right').toggle('shake');
+  });
+
+    
 
 })
