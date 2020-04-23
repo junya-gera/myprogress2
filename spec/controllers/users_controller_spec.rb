5708261e0d0ece32f0769d58c176d2f06ac42bec
junya-gera <aa106921@gmail.com>
2020-04-23 23:43:39 +0900

    fix GET-edit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 describe UsersController do
   describe 'GET #edit' do
+    it "@tweetに正しい値が入っていること" do
+    end
+
+
     it "edit.html.erbに遷移すること" do
       get :edit,
       expect(response).to render_template :edit
-
     end
   end
 
