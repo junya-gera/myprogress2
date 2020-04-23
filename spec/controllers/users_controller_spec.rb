b99afaef2d023875efb77840de81197e312ea91e
junya-gera <aa106921@gmail.com>
2020-04-23 23:41:54 +0900

    fix GET-edit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe UsersController do
   describe 'GET #edit' do
     it "edit.html.erbに遷移すること" do
+      get :edit,
+      expect(response).to render_template :edit
+
     end
   end
 
