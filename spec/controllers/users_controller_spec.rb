7c1760bcdbac038e7407bba0944326a5175aa9b6
junya-gera <aa106921@gmail.com>
2020-04-24 00:06:03 +0900

    add GET-show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe UsersController do
+
   describe 'GET #edit' do
     it "@userに正しい値が入っていること" do
       user = create(:user)
@@ -8,13 +9,28 @@ describe UsersController do
       expect(assigns(:user)).to eq user
     end
 
-
     it "edit.html.erbに遷移すること" do
       user = create(:user)
       get :edit, params: { id: user }
       expect(response).to render_template :edit
     end
   end
+
+
+  describe 'GET #show' do
+    it "@userに正しい値が入っていること" do
+      user = create(:user)
+      get :show, params: { id: user }
+      expect(assigns(:user)).to eq user
+    end
+
+    it "show.html.erbに遷移すること" do
+      user = create(:user)
+      get :show, params: { id: user }
+      expect(response).to render_template :edit
+    end
+  end
+
 
   describe 'GET #index' do
     it "@usersに正しい値が入っていること" do
