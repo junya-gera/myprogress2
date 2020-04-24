90da4d129c745303f13dc3f40fb6783214ce8eff
junya-gera <aa106921@gmail.com>
2020-04-24 21:52:51 +0900

    add controllerMacros to config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include ControllerMacros, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :request
