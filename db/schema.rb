e2585886df77257018bd5962fc6cbdea736ca752
junya-gera <aa106921@gmail.com>
2020-05-02 21:12:57 +0900

    add default to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_061239) do
+ActiveRecord::Schema.define(version: 2020_05_02_120353) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_061239) do
     t.datetime "updated_at", null: false
     t.boolean "square", default: false, null: false
     t.bigint "user_id"
-    t.string "task"
+    t.string "task", default: "タスクが入力されていません"
     t.index ["user_id"], name: "index_puzzles_on_user_id"
   end
 
