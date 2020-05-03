1623de925e31b92653afd4979d0ee9685812772c
junya-gera <aa106921@gmail.com>
2020-05-03 10:28:52 +0900

    delete is_puzzle? method in Puzzle model

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,12 +1,4 @@
 class Puzzle < ApplicationRecord
   belongs_to :user
 
-  def self.is_puzzle?
-    if @puzzles == nil
-      9.times do
-        Puzzle.create(user_id: @user.id)
-      end
-    end
-  end
-
 end
