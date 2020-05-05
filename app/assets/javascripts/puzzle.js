750c4bbc945faf354a8255fc5c9e6356844a36fc
junya-gera <aa106921@gmail.com>
2020-05-05 15:50:44 +0900

    delete task-modal-show in puzzle.js

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -1,14 +1,5 @@
 $(document).on("turbolinks:load", function() {
 
-  $('#task-modal-show').on('click', function(){
-    $('.task-modal').fadeIn(250);
-  })
-
-  $('#modal-close').on('click', function(){
-    $('.task-modal').fadeOut(250);
-  })
-
-
   const image = new Image();
   image.onload = function(e){
     const canvas = document.getElementById("stage");
