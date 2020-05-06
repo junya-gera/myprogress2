3d8b0210c37d3e70b5100493efd0e064cf95651f
junya-gera <aa106921@gmail.com>
2020-05-06 19:11:45 +0900

    delete task default

--- db/migrate/20200506100848_change_task_default.rb
+++ db/migrate/20200506100848_change_task_default.rb
@@ -1,9 +1,9 @@
 class ChangeTaskDefault < ActiveRecord::Migration[5.2]
   def up
-    change_column :puzzles, :task, :string, default: "タスクが入力されていません"
+    change_column :puzzles, :task, :string
   end
 
   def down
-    change_column :puzzles, :task, :string
+    change_column :puzzles, :task, :string, default: "タスクが入力されていません"
   end
 end
