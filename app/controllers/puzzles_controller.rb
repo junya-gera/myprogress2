c4dd115fdaaa101c806eb31dc07cd1180dd96bc7
junya-gera <aa106921@gmail.com>
2020-05-08 23:06:27 +0900

    fix puzzles-index

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -2,7 +2,7 @@ class PuzzlesController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @puzzles = Puzzle.where(user_id: current_user)
+    @puzzles = Puzzle.where(user_id: @user)
   end
 
   def edit
